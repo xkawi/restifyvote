7eb0001525ecd7404471b8e9c3263a96f6243f68
Kawi <neocrimson58@gmail.com>
2014-03-29 14:39:41 +0800

    Add start true or false

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var restifyvote = angular.module("restifyvote", ["firebase", "timer"]);
 
-/*restifyvote.factory('statusService', ["$firebase", function ($firebase) {
+restifyvote.factory('statusService', ["$firebase", function ($firebase) {
     var startRef = new Firebase("https://restifyvote.firebaseio.com/start");
     return $firebase(startRef);
-}]);*/
+}]);
 
-restifyvote.controller("MainController", ["$scope", "$firebase",
-    function($scope, $firebase){
+restifyvote.controller("MainController", ["$scope", "$firebase", "statusService",
+    function($scope, $firebase, statusService){
         //var playerRef = new Firebase("https://restifyvote.firebaseio.com/player_information");
         //var attackRef = new Firebase("https://restifyvote.firebaseio.com/attack_methods");
         //$scope.players = $firebase(playerRef);
         //$scope.attacks = $firebase(attackRef);
-        //statusService.$bind($scope, "start");
+        statusService.$bind($scope, "start");
 
         $scope.rootRef = $firebase(new Firebase("https://restifyvote.firebaseio.com"));
         
